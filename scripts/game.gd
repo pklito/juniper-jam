5dc047c20625f1881cc9caef3d7d51899337432e
Paulo Khayat <paulokhayat@gmail.com>
2026-06-21 02:25:49 -0400

    Transfer calculations to a class

--- scripts/game.gd
+++ scripts/game.gd
@@ -5,18 +5,46 @@ extends Node2D
 @export var tile_size: int = 128
 @export var canvas_width: int = 1152
 @export var canvas_height: int = 648
-
-
 @export var target_tiles_wide: int = 18
-@onready var target_tiles_high: int = ceil(target_tiles_wide * canvas_height / float(canvas_width))
 
-@onready var br_corner :Vector2i = Vector2i(target_tiles_wide - 1, target_tiles_wide - 1)
+enum TileType{
+	NONE,
+	GROUND
+}
+var _tile_config : TileSetConfig
+class TileSetConfig:
+	var width : int
+	var height : int
+	var tile_pixels : int
+	var canvas_size : Vector2i
+	var pixel_scale : float
 
+	func _init(_tile_pixels:int, _canvas_size:Vector2i, _target_width : int ) -> void:
+		width = _target_width
+		tile_pixels = _tile_pixels
+		canvas_size = _canvas_size
+		pixel_scale = float(_canvas_size.x) / (_target_width * _tile_pixels)
+		height = ceil(canvas_size.y / (pixel_scale * _tile_pixels))
+	
+	func get_tile_type(layer : TileMapLayer, pos: Vector2i) -> TileType:
+		var tile_id = layer.get_cell_atlas_coords(pos)
+		print("Tile ID at position ", pos, ": ", tile_id)
+		match tile_id:
+			0:
+				return TileType.NONE
+			_:
+				return TileType.GROUND
 
-# Called when the node enters the scene tree for the first time.
+# Special func
 func _ready() -> void:
-	tiles.scale = float(canvas_width) / (target_tiles_wide * tile_size) * Vector2(1,1)
-	pass # Replace with function body.
+	# setup
+	_tile_config = TileSetConfig.new(tile_size, Vector2i(canvas_width, canvas_height), target_tiles_wide)
+	tiles.scale = _tile_config.pixel_scale * Vector2(1,1)
+	
+	for x in range(_tile_config.width):
+		for y in range(_tile_config.height):
+			var tile_type = _tile_config.get_tile_type(tiles, Vector2i(x,y))
+	
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
