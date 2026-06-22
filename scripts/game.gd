5736941a743c0e1726647bdaabf10ea303e27654
Paulo Khayat <paulokhayat@gmail.com>
2026-06-21 23:57:27 -0400

    Start intraplayer collision

--- scripts/game.gd
+++ scripts/game.gd
@@ -28,6 +28,7 @@ class Player:
 	var player_node : Node2D
 	var tile_config : TileSetConfig
 	var tiles : TileMapLayer
+	var other_player: Player
 	func _init(_tile_config : TileSetConfig, _tiles : TileMapLayer, _player_node: Node2D, _pos: Vector2i, _ground_dir: int) -> void:
 		pos = _pos
 		ground_dir = _ground_dir
@@ -35,18 +36,27 @@ class Player:
 		tile_config = _tile_config
 		tiles = _tiles
 
+	func link(_other_player: Player):
+		other_player = _other_player
+		_other_player.other_player = self
+
+	func _pos_solid_tile(test_pos: Vector2i) -> bool:
+		return tile_config.get_tile_type(tiles, test_pos) != Utils.TileType.NONE
 	
 	func _pos_open(test_pos: Vector2i) -> bool:
-		return tile_config.get_tile_type(tiles, test_pos) == Utils.TileType.NONE
+		return not _pos_solid_tile(test_pos) and other_player.pos != test_pos
+
 	func _move_internal(move_vec: Vector2i) -> void:
 		if _pos_open(pos + move_vec):
-			if not _pos_open(pos + move_vec + Utils.dir_to_vec2(ground_dir)):
+			if _pos_solid_tile(pos + move_vec + Utils.dir_to_vec2(ground_dir)):
 				pos = pos + move_vec
 				# TODO: handle double turn
 			else:
 				pos = pos + move_vec + Utils.dir_to_vec2(ground_dir)
 				ground_dir = Utils.vec2_to_dir(-move_vec)# turn around
-				
+
+	func _post_check(_a : Vector2i):
+		pass			
 
 	func update_graphics() -> void:
 		player_node.position = tile_config.pos_to_pixel(pos)
@@ -54,9 +64,11 @@ class Player:
 	func move_left() -> void:
 		var test_dir = posmod(ground_dir + 1, 4)
 		_move_internal(Utils.dir_to_vec2(test_dir))
+		_post_check(Utils.dir_to_vec2(test_dir))
 	func move_right() -> void:
 		var test_dir = posmod(ground_dir - 1, 4)
 		_move_internal(Utils.dir_to_vec2(test_dir))
+		_post_check(Utils.dir_to_vec2(test_dir))
 
 class PlayerSquare extends Player:
 	func update_rotation(going_left: bool):
@@ -65,12 +77,19 @@ class PlayerSquare extends Player:
 			move_dir = posmod(ground_dir - 1, 4)
 		var move_vec := Utils.dir_to_vec2(move_dir)
 		# print(move_vec, " ", pos + move_vec, " ", move_dir, " ", _pos_open(pos + move_vec))
-		if not _pos_open(pos + move_vec):
+		if _pos_solid_tile(pos + move_vec):
 			ground_dir = move_dir
 	pass
 
+
 class PlayerTriangle extends Player:
-	pass
+	func _pos_solid_tile(test_pos: Vector2i) -> bool:
+		return super._pos_solid_tile(test_pos) or other_player.pos == test_pos
+
+	func _post_check(_move_vec : Vector2i):
+		if not _pos_open(pos + _move_vec):
+			ground_dir = Utils.vec2_to_dir(_move_vec)
+
 	
 		
 
@@ -83,7 +102,8 @@ func _ready() -> void:
 	
 	square = PlayerSquare.new(_tc, tiles, player_sq_node, Vector2i(5,3), 0)
 	triangle = PlayerTriangle.new(_tc, tiles, player_tri_node, Vector2i(8,5), 0)
-	
+	square.link(triangle)
+
 	square.update_graphics()
 	triangle.update_graphics()
 var time = 0.0
@@ -97,8 +117,8 @@ func _process(delta: float) -> void:
 	time2 += delta
 	if time > 1:
 		time = 0
-		square.move_right()
-		# triangle.move_right()
+		square.move_left()
+		triangle.move_right()
 	if time2 > 0.1:
 		time2 = 0
-		square.update_rotation(false)
+		square.update_rotation(true)
